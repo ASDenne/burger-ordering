0173e035e002250614acab8a839f514d93b16fc9
ASDenne <98923598+ASDenne@users.noreply.github.com>
2023-03-28 12:16:15 +1300

    program picker alternite

--- program picker.py
+++ program picker.py
@@ -1,10 +1,11 @@
 import easygui
-
-options = ["print test","print hello world","exit"]
+def test_1():
+    print("test output 1")
+def test_2():
+    print("test output 2")
+options = ["test input 1","test input 2","exit"]
+action_defs = [test_1, test_2, exit]
 action = easygui.buttonbox("what are you trying to do?",choices=options)
-if action == "print test":
-    print("test")
-if action == "print hello world":
-    print("hello world")
-
-
+for i in range(0,len(options)):
+    if action == options[i]:
+        action_defs[i]()
