28161ead42d814658a090e7717d909ef53afc34f
ASDenne <98923598+ASDenne@users.noreply.github.com>
2023-04-20 14:21:54 +1200

    deleat function working

--- deleat.py
+++ deleat.py
@@ -1,3 +1,29 @@
 import easygui
-def deleat():
-    deleat = easygui.enterbox("")
+def delete():
+    delete = easygui.enterbox("what input do you want to delete?")
+    if delete in menus:
+        menus.pop(delete)
+        easygui.msgbox(f"{delete} deleted")
+    else:
+        easygui.msgbox(f"{delete} not found could not delete")
+
+
+menus = {
+    "value": {
+        "beef burger": 5.69,
+        "fries": 1.00,
+        "fizzy drink": 1.00
+    },
+    "cheezy": {
+        "cheeseburger": 6.69,
+        "fries": 1.00,
+        "fizzy drink": 1.00
+    },
+    "super": {
+        "cheeseburger": 6.69,
+        "large fries": 2.00,
+        "smoothie": 2.00
+    }
+}
+delete()
+print(menus)
--- main.py
+++ main.py
@@ -1,59 +1,67 @@
 import easygui
+
+
 def format_menu(menu):
     menu_text = f"take away menu\n" \
                 "\n\n" \
-                "items\n\n"
+                "items\n\n"  # starts writen menu with title
     for combo_name, combo_elements in menu.items():
-        combo_menu = ""
-        combo_cost = 0
+        combo_menu = ""  # starts writen combo
+        combo_cost = 0  # sets combo cost to 0
         for item_name, item_price in combo_elements.items():
-            combo_menu += f"    {item_name} -${item_price}-\n"
-            combo_cost += item_price
-        menu_text += f"{combo_name} -${round(combo_cost,4)}-\n\n{combo_menu}\n\n"
+            combo_menu += f"    {item_name} -${item_price}-\n"  # adds an item to the  writen combo
+            combo_cost += item_price  # adds the cost of the item to the writen cost
+        menu_text += f"{combo_name} -${round(combo_cost,4)}-\n\n{combo_menu}\n\n"  # add info about the combo to the writen menu
     return menu_text
 def float_checker(text):
     float_ = ""
-    while float_ == "":
+    while float_ == "":  # countiues till got float
         try:
-            float_ = float(easygui.enterbox(text))
+            float_ = float(easygui.enterbox(text))  # trying to get a float
             break
         except ValueError :
-            easygui.msgbox("please enter a number")
+            easygui.msgbox("please enter a number")  # ask again if fails
     return float_
 def add_combo():
-    combo_name = easygui.enterbox("what do you want to call your combo?")
-    combo_size = easygui.integerbox("how many item's in combo")
+    combo_name = easygui.enterbox("what do you want to call your combo?")#get combo name
+    combo_size = easygui.integerbox("how many item's in combo")#get how big it is
     combo = {}
     for i in range(0,combo_size):
-        item_name = easygui.enterbox(f"what is item number {i+1}?")
-        item_price = float_checker(f"how much does {item_name} cost?")
-        combo[item_name] = item_price
-    menus[combo_name] = combo
-    print(menus)
+        item_name = easygui.enterbox(f"what is item number {i+1}?")#getting a item
+        item_price = float_checker(f"how much does {item_name} cost?")#getting it's price
+        combo[item_name] = item_price#adding them item to the combo
+    menus[combo_name] = combo#adding the combo to the menu
 def print_whole_menu():
-    easygui.msgbox(format_menu(menus))
+    easygui.msgbox(format_menu(menus))#printing writen menu
 def search_menu():
-    search = easygui.enterbox("please enter combo name")
-    if search in menus:
-        combo_menu = ""
+    search = easygui.enterbox("please enter combo name")#finding out what thier after
+    if search in menus:#if it's found
+        combo_menu = ""#print the combo ,next 5 lines
         combo_cost = 0
         for item_name, item_price in menus[search].items():
             combo_menu += f"    {item_name} -${item_price}-\n"
             combo_cost += item_price
-        while easygui.buttonbox(f"Is this correct? \n{search} -${round(combo_cost,4)}-\n\n{combo_menu}\n\n",choices=["Yes","No"]) == "No":
+        while easygui.buttonbox(f"Is this correct? \n{search} -${round(combo_cost,4)}-\n\n{combo_menu}\n\n",choices=["Yes","No"]) == "No":#countinue while combo has a mistake in pricing
             combo_items = []
             for item, combo in menus[search].items():
-                combo_items.append(item)
-            fix = easygui.buttonbox("what's priced wrong?",choices=combo_items)
-            right = float_checker("what should it cost?")
-            menus[search][fix] = right
-            combo_menu = ""
+                combo_items.append(item)#listing combo items
+            fix = easygui.buttonbox("what's priced wrong?",choices=combo_items)#asking where the mistake is
+            right = float_checker("what should it cost?")#asking how to fix it
+            menus[search][fix] = right#fixing it
+            combo_menu = ""#printing menu again
             combo_cost = 0
             for item_name, item_price in menus[search].items():
                 combo_menu += f"    {item_name} -${item_price}-\n"
                 combo_cost += item_price
-    else:
-        search_menu()
+    else:#if it's not found
+        search_menu()#restart search menu
+def delete():
+    delete = easygui.enterbox("what input do you want to delete?")#getting what do you want removed
+    if delete in menus:#if it's thier
+        menus.pop(delete)#deleate it
+        easygui.msgbox(f"{delete} deleted") #tell them you have
+    else: # other wise
+        easygui.msgbox(f"{delete} not found could not delete")#tell them you can't find it and return to menu
 
 menus = {
     "value": {
@@ -71,10 +79,10 @@ menus = {
         "large fries": 2.00,
         "smoothie": 2.00
     }
-}
-options = ["print whole menu","add_combo","search menu","exit"]
-action_defs = [print_whole_menu, add_combo, search_menu, exit]
-action = easygui.buttonbox("what are you trying to do?",choices=options)
+}#set's up menu
+options = ["print whole menu","add_combo","search menu","delete menu item","exit"]#buttons
+action_defs = [print_whole_menu, add_combo, search_menu, delete, exit]#functions connected to above buttons
+action = easygui.buttonbox("what are you trying to do?",choices=options)#ask what action they want to do
 for i in range(0,len(options)):
-    if action == options[i]:
-        action_defs[i]()
+    if action == options[i]:#checks if action choosen
+        action_defs[i]()#preforms action
